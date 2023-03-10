06118a208fc55d8336b3cf39dc6fbf0f4d629fc6
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-03-10 15:50:55 -0700

    Fix duplication when adding to hash map

--- hash_map.py
+++ hash_map.py
@@ -1,28 +1,25 @@
 class HashMap:
     def __init__(self):
-        self.size = 128
+        self.size = 10
         self.map = [None] * self.size
 
     # Hash a key
     def __get_hash(self, key):
-        hashed_key = 0
-        for char in str(key):
-            hashed_key += ord(char)
-        return hashed_key % self.size
+        return int(key) % self.size
 
     # Add a key-value pair to the hash table or update an existing one
     def add(self, key, value):
         hashed_key = self.__get_hash(key)
-        key_value = [key, value]
+        new_key_value = [key, value]
 
         if self.map[hashed_key] is None:
-            self.map[hashed_key] = list([key_value])
+            self.map[hashed_key] = list([new_key_value])
             return True
         for key_value in self.map[hashed_key]:
             if key_value[0] == key:
                 key_value[1] = value
                 return True
-        self.map[hashed_key].append(key_value)
+        self.map[hashed_key].append(new_key_value)
         return True
 
     # Delete a key-value pair from the hash table
