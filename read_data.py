ca79beca914f967015b7a6d53866305653d1d60e
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-03-11 10:17:38 -0700

    Add distance data and put in adjacency matrix

--- read_data.py
+++ read_data.py
@@ -6,3 +6,9 @@ with open('./data/package_info.csv') as package_csv:
     package_reader = csv.reader(package_csv, delimiter=',')
     for package in package_reader:
         hash_map.add(package[0], package)
+
+with open('./data/distances.csv') as distance_csv:
+    distance_matrix = []
+    distance_reader = csv.reader(distance_csv, delimiter=',')
+    for address in distance_reader:
+        distance_matrix.append(address)
